374484d0101b00eac960c1fadc6598f16d3c8b01
Daria <darya.shadrina95@gmail.com>
2023-02-13 18:54:25 +0200

    Add task002

--- Task002/Program.cs
+++ Task002/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача со сравнением чисел
+Console.WriteLine("Enter numberA:");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter numberB:");
+int numberB = int.Parse(Console.ReadLine());
+if (numberA == numberB)
+{
+    Console.WriteLine("A = B");
+}
+else
+{
+    if (numberA > numberB)
+    {
+        Console.WriteLine("A is greater than B");
+    }
+    else
+    {
+        Console.WriteLine("B is greater than A");
+    }
+}
