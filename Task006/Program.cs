0e641e62c2e10a0b40ccc96278375b0fd9f80b98
Daria <darya.shadrina95@gmail.com>
2023-02-13 19:06:01 +0200

    Add task006

---
+++ Task006/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 6 - Проверка четное-нечетное
+Console.WriteLine("Enter the number:");
+int num1 = int.Parse(Console.ReadLine());
+if (num1 % 2 == 0)
+{
+    Console.WriteLine("The number is even");
+}
+else
+{
+    Console.WriteLine("The number is odd");
+}
