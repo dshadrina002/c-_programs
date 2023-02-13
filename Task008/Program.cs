f33a65772ec1094d4ef2ba10bab1a9a5ec4fa70f
Daria <darya.shadrina95@gmail.com>
2023-02-13 19:14:39 +0200

    Add task008

---
+++ Task008/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 8 -  Вывод четных чисел от 1 до N
+Console.Write("Enter the number: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("The even numbers from 1 to " + num1 + " are: ");
+for (int i = 2; i <= num1; i = i + 2)
+{
+    Console.Write(i + " ");
+}
