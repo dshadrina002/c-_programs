80f2005c43e446b30b11aa3750966e8f6add66f5
Daria <darya.shadrina95@gmail.com>
2023-02-20 16:17:09 +0200

    Add task 23

---
+++ Task023/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 23. Вывод таблицы кубов от 1 до N
+Console.Write("Enter the number: ");
+int N = int.Parse(Console.ReadLine()!);
+for (int i=1; i <= N; i++)
+{
+    Console.Write(Math.Pow(i,3) + " ");
+}
