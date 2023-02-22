7cc58e2009c031e479d06cd12d2dcdb77e8c9e39
Daria <darya.shadrina95@gmail.com>
2023-02-22 11:25:47 +0200

    Added Task29-30

--- Task030/Program.cs
+++ Task030/Program.cs
@@ -1,15 +1,19 @@
 ﻿void FillArray(int[] collection)
 {
     for(int i = 0; i < collection.Length; i++)
-    collection[i] = new Random().Next(0, 2);
+    collection[i] = new Random().Next(0, 1000);
 }
 
 void PrintArray(int[] col)
 {
+    Console.Write("[");
     for(int i = 0; i < col.Length; i++)
-    Console.Write($"{String.Join(", ", col[i])}");
+    {
+        Console.Write($"{col[i]}, ");
+    }
+    Console.Write("]");
 }
-Console.WriteLine("Enter the array length: ");
+Console.Write("Enter the array length: ");
 int N = int.Parse(Console.ReadLine()!);
 int [] array = new int[N];
 Console.WriteLine("Your array: ");
