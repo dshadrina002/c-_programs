ffa34b26e978859aada6ba926a8e0ec5a76a1730
Daria <darya.shadrina95@gmail.com>
2023-02-22 11:12:05 +0200

    Added Task027

---
+++ Task027/Program.cs
@@ -0,0 +1,14 @@
+﻿int DigitSum(int n)
+{
+    int sum = 0;
+    while (n > 0)
+    {
+        sum = sum + n % 10;
+        n = n/10;
+    }
+    return sum;
+}
+Console.Write("Enter the number: ");
+int A = int.Parse(Console.ReadLine()!);
+int res = DigitSum(A);
+Console.Write($"Sum of {A} digits is {res}");
