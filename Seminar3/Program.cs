dabfa426063265782e415a2177ea1fb16ee5942e
Daria <darya.shadrina95@gmail.com>
2023-02-27 18:02:35 +0200

    Add seminar3

---
+++ Seminar3/Program.cs
@@ -0,0 +1,39 @@
+﻿// заполняем массив
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for(int i = 0; i<size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue+1);
+    }
+    return res;
+}
+
+void PrintArray(int[] inArray)
+{
+    for(int i = 0; i<inArray.Length; i++)
+    {
+        Console.Write($"{inArray[i]} ");
+    }   
+}
+
+int[] array = GetArray(12, -9, 9);
+PrintArray(array);
+
+int positiveSum = 0;
+int negativeSum = 0;
+
+foreach(int el in array)
+{
+    if(el>0)
+    {
+        positiveSum +=el;
+    }
+    else
+    {
+        negativeSum +=el;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"Сумма положительных чисел {positiveSum}, Сумма отрицательных элементов {negativeSum}");
